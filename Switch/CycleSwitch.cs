1bfca7a41243196d2680ce90e3b333a611051eda
PeaceKunihiro <sinti525kunihiro@gmail.com>
2026-03-12 07:30:15 +0900

    summary
    Contoroll to object cycle selector.
    任意数の制御対象オブジェクトを順次切り替えていくスイッチです。

---
+++ Switch/CycleSwitch.cs
@@ -0,0 +1,54 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CycleSwitch : UdonSharpBehaviour
+{
+    [Header("切り替え対象")]
+    public GameObject[] targets;
+    //操作対象Object用配列　1次元配列
+
+    [Header("初期表示オブジェクト")]
+    public int startIndex = 0;
+    //状態同期用
+    [UdonSynced]
+    private int currentIndex = 0;
+
+
+    void Start()
+    {
+        currentIndex = startIndex;
+        UpdateObjects();
+    }
+    public override void Interact()
+    {
+        if(targets == null || targets.Length ==0) return;//制御対象が無ければ実行しない
+
+        Networking.SetOwner(Networking.LocalPlayer,gameObject);
+
+        currentIndex++;
+        if(currentIndex >= targets.Length)
+        {
+            currentIndex =0;
+        }
+        UpdateObjects();
+        RequestSerialization();
+    }
+
+    public override void OnDeserialization()
+    {
+        UpdateObjects();
+    }
+    private void UpdateObjects()
+    {
+    for(int i = 0; i < targets.Length; i++)
+        {
+            if(targets[i] != null)
+            {
+                targets[i].SetActive(i == currentIndex);
+            }
+        }
+    }
+}

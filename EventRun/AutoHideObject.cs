6783e167978ac9388f3b0a9fff3dca4a1e479128
PeaceKunihiro <sinti525kunihiro@gmail.com>
2026-04-01 20:48:12 +0900

    Create AutoHideObject.cs
    Avtiveになると設定時間後に自身をオフにするUdon

---
+++ EventRun/AutoHideObject.cs
@@ -0,0 +1,29 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class AutoHideObject : UdonSharpBehaviour
+{
+    [Header("有効化後に非表示にするまでの秒数")]
+    public float lifeTime = 5f;
+
+    private float disableAt;
+
+    private void OnEnable()
+    {
+        disableAt = Time.time + lifeTime;
+        SendCustomEventDelayedSeconds(nameof(TryDisable), lifeTime);
+    }
+
+    public void TryDisable()
+    {
+        float remain = disableAt - Time.time;
+
+        if (remain > 0.01f)
+        {
+            SendCustomEventDelayedSeconds(nameof(TryDisable), remain);
+            return;
+        }
+
+        gameObject.SetActive(false);
+    }
+}
